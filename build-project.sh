de6b9a51ad4456c806e6ca7fcf153cc439aae987
jyoh13702 <jyoh13702@gmail.com>
2025-12-04 17:15:19 -0500

    script attempt 1

---
+++ build-project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This is my first Bash lab!
+projectName=$1
+mkdir projectName
+cd projectName
+touch README.md
+touch projectName.sh
+git init
+git add .
+git commit -m "initial commit"
+chmod +x projectName.sh

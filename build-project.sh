364f4098f0a7f057f717f6f6239dafbeaf1aa16a
jyoh13702 <jyoh13702@gmail.com>
2025-12-04 17:18:23 -0500

    fix variable

--- build-project.sh
+++ build-project.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This is my first Bash lab!
 projectName=$1
-mkdir projectName
-cd projectName
+mkdir $projectName
+cd $projectName
 touch README.md
-touch projectName.sh
+touch $projectName.sh
 git init
 git add .
 git commit -m "initial commit"
-chmod +x projectName.sh
+chmod +x $projectName.sh
